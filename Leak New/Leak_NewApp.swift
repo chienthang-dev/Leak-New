457bffb0abf1343b0bbd7a12411985e75c59ba47
vuchienthang <thangveo.cp99@gmail.com>
2023-01-02 13:33:26 +0300

    Initial Commit

---
+++ Leak New/Leak_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leak_NewApp.swift
+//  Leak New
+//
+//  Created by Vũ Chiến Thắng on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Leak_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
