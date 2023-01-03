dd842155a3fe68ffdf305b94ac18482e77875746
vuchienthang <thangveo.cp99@gmail.com>
2023-01-03 17:23:12 +0300

    show news on UI

--- Leak New/ContentView.swift
+++ Leak New/ContentView.swift
@@ -8,19 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+   @ObservedObject var networkManager = NetworkManager()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
+                
+                
+            }
+            .navigationTitle("Leak New")
+           
         }
-        .padding()
+        .onAppear {
+            self.networkManager.fetchData()
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
